50562aa5f4fc753f20fb4548400a5beedc531562
petot <petot@abs.com>
2020-10-12 15:42:38 +0700

    petot

---
+++ python-fundamental/002.Variable/variable.py
@@ -0,0 +1,30 @@
+# varible merupakan tempat untuk menyimpan nilai / value
+
+# penamaan variable pertama
+
+a = 20
+
+# pemanggilan varible
+
+
+
+#  penamaan variable kedua
+
+nama = "Abubakar sidik"  # variable nama menggunakan huruf kecil
+Nama = "Abubakar sidik"  # variable nama diawali huruf besar
+# 10juta = "uang" # variable tidak boleh diawali angka
+_nama = "saya" # varible nama bisa diawali underscore
+juta10 = 1000000 # varible bisa diberikan angka dibelakang huruf
+
+# pemanggilan variable kedua
+
+print(nama)
+print(Nama)
+print(_nama)
+print(juta10)
+
+# variable indirect / menimpa nilai varible
+
+a = 30
+b = a
+print(b)
