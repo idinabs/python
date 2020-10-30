e5902b20a70d4bca3dbf741cf6ca4f1de47f7833
petot <idinabs@gmail.com>
2020-10-30 09:49:39 +0700

    petot

---
+++ python-fundamental/020.fo-range-else-breake/main.py
@@ -0,0 +1,10 @@
+angka = 2
+
+for i in range(0,30): #mengecel rentang nilai
+    print(i)
+    if i == 2: #mengecek nilai yang ada di range
+        print("ini adalah",angka) 
+        break  #akan menghentikan semua proses setelahnya
+
+else:
+    print("tidak ada angka disini") #jika nilai didalam variabel tidak ada maka akan jalan
