8d4aaf10efba01c7257a836af540d133800f5519
petot <idinabs@gmail.com>
2020-10-30 10:10:21 +0700

    petot

---
+++ python-fundamental/021.continue-pass/main.py
@@ -0,0 +1,12 @@
+
+for i in range(0,30): #mengecel rentang nilai
+    if i == 4: #mengecek nilai yang ada di range
+        print("ini adalah",4) 
+        #break  #akan menghentikan semua proses setelahnya
+        #continue #akan melanjutkan ke step berikutnya
+        pass
+        print("oke siap")
+    print("oke siap bro")
+
+else:
+    print("tidak ada angka disini") #jika nilai didalam variabel tidak ada maka akan jalan
